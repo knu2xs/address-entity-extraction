e6e566338666a481244ba6c2ccff6a05197a9f03
Joel McCune <knu2xs@gmail.com>
2023-07-20 12:04:27 -0700

    initial commit

---
+++ scripts/make_data.py
@@ -0,0 +1,41 @@
+"""
+Licensing
+
+Copyright 2020 Esri
+
+Licensed under the Apache License, Version 2.0 (the "License"); You
+may not use this file except in compliance with the License. You may
+obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing
+permissions and limitations under the License.
+
+A copy of the license is available in the repository's
+LICENSE file.
+"""
+import pkgutil
+
+# if the project package is not installed in the environment
+if pkgutil.find_loader('address_entity_extraction') is None:
+    
+    # late imports for finding the package relative to the script
+    from pathlib import Path
+    import sys
+    
+    # get the relative path to where the source directory is located
+    src_dir = Path(__file__).parent.parent / 'src'
+
+    # throw an error if the source directory cannot be located
+    if not src_dir.exists():
+        raise EnvironmentError('Unable to import address_entity_extraction.')
+
+    # add the source directory to the paths searched when importing
+    sys.path.insert(0, str(src_dir))
+
+# import the project package
+import address_entity_extraction
